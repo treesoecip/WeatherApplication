59b0c65de534906280906a77bb173427150e924d
treesoecip <prasetyotri2012@gmail.com>
2018-03-22 09:21:43 +0800

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import VolumeBalok from './src/latihan/volumeBalok';
+//import VolumeBalok from './src/latihan/volumeBalok';
 //import Header from './src/Cuaca/Header';
-//import BelajarLayout from './src/comp/BelajarLayout';
+import BelajarLayout from './src/comp/BelajarLayout';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.containerMain}>
-          <VolumeBalok />
+          <BelajarLayout />
       </View>
 
     );
